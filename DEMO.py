1f707ebf45d177ee4206c447621365c0ed5b229c
TruongKhoaNguyen <huynhbichtran050185@gmail.com>
2024-12-15 18:13:05 +0700

    ??
    1+1=3

--- DEMO.py
+++ DEMO.py
@@ -9,12 +9,15 @@ class Demo(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("UI-GiaoDien/homepage.ui",self)
-        #self.btnProfile.clicked.connect(self.showProfile)
         self.btnPlay_PP.clicked.connect(lambda: self.mainStack.setCurrentIndex(1))
         self.btnPlay_PVZ.clicked.connect(lambda: self.mainStack.setCurrentIndex(2))
         self.btnPlay_RB.clicked.connect(lambda: self.mainStack.setCurrentIndex(3))
         self.btnPlay_GD.clicked.connect(lambda: self.mainStack.setCurrentIndex(4))
-        self.btnPlay_GD.clicked.connect(lambda: self.mainStack.setCurrentIndex(4))
+        self.btnPlay_FF.clicked.connect(lambda: self.mainStack.setCurrentIndex(5))
+        self.btnPlay_M.clicked.connect(lambda: self.mainStack.setCurrentIndex(6))
+        self.btnPlay_BMW.clicked.connect(lambda: self.mainStack.setCurrentIndex(7))
+        self.btnPlay_A.clicked.connect(lambda: self.mainStack.setCurrentIndex(8))
+        self.btnHome.clicked.connect(lambda: self.mainStack.setCurrentIndex(0))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
